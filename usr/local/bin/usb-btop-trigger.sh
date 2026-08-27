6e075d6e9ac81af9e79e6262e2a16c9b1186f912
Empy-ai09 <ramadhanzahwan98@gmail.com>
2026-08-27 14:22:55 +0700

    update

--- usr/local/bin/usb-btop-trigger.sh
+++ usr/local/bin/usb-btop-trigger.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 set -eu
-runuser -u "$SUDO_USER" -- systemctl --user start usb-btop.service
+
+# Detect active user
+USER_NAME=${SUDO_USER:-${LOGNAME:-${USER:-""}}}
+if [ -z "$USER_NAME" ]; then
+  exit 0
+fi
+
+# Pass device node from udev (DEVNAME)
+DEVNODE=${DEVNAME:-""}
+
+runuser -u "$USER_NAME" -- systemctl --user start usb-btop@"${DEVNODE}".service
